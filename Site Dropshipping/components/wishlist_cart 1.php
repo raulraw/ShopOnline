004378c618b27f67f8e54fbecc2227b3110d653c
Staicu Raul-Valentin <raul_valentin123@yahoo.com>
2023-03-01 11:03:05 +0200

    Add files via upload

---
+++ Site Dropshipping/components/wishlist_cart 1.php
@@ -0,0 +1,99 @@
+ please !</p>
+            </div>
+
+            <div class="box" data-aos="fade-right">
+               <span>( 2021 - 2022 )</span>
+               <h3>Graphic designer</h3>
+               <p>Not yet</p>
+            </div>
+
+         </div>
+
+         <div class="box-container">
+
+            <h3 class="title" data-aos="fade-left">Experience</h3>
+
+            <div class="box" data-aos="fade-left">
+               <span>( 2021 - 2022 )</span>
+               <h3>Front-end Developer</h3>
+               <p>Intermediate level, to be continued...</p>
+            </div>
+
+            <div class="box" data-aos="fade-left">
+               <span>( 2021 - 2022 )</span>
+               <h3>Back-end Developer</h3>
+               <p>Still learning on it, to be continued...</p>
+            </div>
+
+            <div class="box" data-aos="fade-left">
+               <span>( 2021 - 2022 )</span>
+               <h3>Full-stack Developer</h3>
+               <p>Still working on it, dreams come true, to be continued...</p>
+            </div>
+
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+   <h1 class="heading" data-aos="fade-up"> <span>Services</span> </h1>
+
+   <div class="box-container">
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-code"></i>
+         <h3>Web Development</h3>
+         <p>My hobby & passion</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fas fa-paint-brush"></i>
+         <h3>CSS</h3>
+         <p>I think that's the digital art of a programmer, just give life to your site!</p>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <i class="fab fa-bootstrap"></i>
+         <h3>Bootstrap</h3>
+         <p>Good CSS Library</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- portfolio section starts  -->
+
+<section class="portfolio" id="portfolio">
+
+   <h1 class="heading" data-aos="fade-up"> <span>Portfolio</span> </h1>
+
+   <div class="box-container">
+
+      <div class="box" data-aos="zoom-in">
+         <img src="images/coffeeshop.png" alt="">
+         <a class="decoration" href="https://rawcoffeeshop.netlify.app/"><h3>Raw Coffee Shop</h3></a>
+         <span>( 2022 )</span>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <img src="images/craiovacars.png" alt="">
+         <a class="decoration" href="https://craiovacars.netlify.app/"><h3>Craiova Cars</h3></a>
+         <span>( 2022 )</span>
+      </div>
+
+      <div class="box" data-aos="zoom-in">
+         <img src="images/shopline.png" alt="">
+         <a class="decoration" href="https://raw-shopline.000webhostapp.com/"><h3>Shopline</h3></a>
+     
